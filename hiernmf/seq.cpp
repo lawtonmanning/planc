1c50e65d26cacaea33d923b3c0ac3d3ba71abf81
John Farrell <farrjt17@wfu.edu>
2020-02-07 10:44:14 -0500

    updated power method to work on A instead of A'A

--- hiernmf/seq.cpp
+++ hiernmf/seq.cpp
@@ -11,7 +11,7 @@ using namespace arma;
 
 
 // returns the maximum eigenvalue of A-transpose * A
-vec powIter (mat A, vec q);
+double powIter (mat A, vec q);
 
 
 /*
@@ -26,40 +26,45 @@ int main () {
 	// UNIT TESTS
 	mat A = randu<mat>(5,5);
 	vec q = randu<vec>(5);
-	cout << A.n_rows << "x" << A.n_cols << " matrix A'A: " << endl;
-	cout << A.t()*A << endl;
-	cout << "Max singular value: \n" << max(svd(A.t()*A)) << endl;
-	cout << (powIter(A,q) - max(svd(A.t()*A))) << endl;	
+	cout << A.n_rows << "x" << A.n_cols << " matrix A: " << endl;
+	cout << A << endl;
+	cout << "Max singular value: \n" << max(svd(A)) << endl;
+	cout << (powIter(A,q) - max(svd(A))) << endl;	
 	return 0;
 }
 
 
-vec powIter (mat A, vec q) {
+/* NOTES:
+ * 
+ * 	Is there a way to change lambda to auto so that it does not need to convert from vector? Hard code as double?
+ *	 
+ */
+double powIter (mat A, vec q) {
 	
 	// normalize q beforehand
 	q = q / norm(q, 2);
 
-	vec lambda, l2;
 	vec z;
+	auto lambda = norm(q,2);
+	auto l2 = lambda;
 	int i = 0;
 	double epsilon = 1.0;
 
 	cout << "\nConvergence: " << endl;
 
 	// powerIteration
-	while (i < 10 && epsilon > 0.000001) {
+	while (i < 10 && epsilon > 0.00001) {
 		z = A * q;
 		q = A.t() * z;
 		q = q / norm(q, 2);
 
 		// store previous lambda for convergence measuring
-		if (i != 0) {
-			l2 = lambda;
-			lambda = z.t() * z;
-			epsilon = as_scalar(lambda - l2);
-		}
+		if (i == 0)
+			lambda = norm(z,2);
 		else {
-			lambda = z.t() * z;
+			l2 = lambda;
+			lambda = norm(z,2);
+			epsilon = abs(lambda - l2)/(lambda);
 		}
 		
 		// track convergence
