5ad6b07619813a0bfe420cf31b18caf5c3b83185
Lawton Manning <mannlg15@wfu.edu>
2020-02-12 12:26:11 -0500

    added driver class

--- hiernmf/hiernmf.cpp
+++ hiernmf/hiernmf.cpp
@@ -1,2 +1,57 @@
-int main() {
+#include "common/parsecommandline.hpp"
+#include "common/utils.h"
+#include "hiernmf/node.hpp"
+
+using namespace planc;
+
+class HierNMFDriver {
+  private:
+    int m_argc;
+    char **m_argv;
+    UWORD m_globalm, m_globaln;
+    std::string m_Afile_name;
+    std::string m_outputfile_name;
+    int m_num_it;
+    int m_pr;
+    int m_pc = 1;
+    FVEC m_regW;
+    FVEC m_regH;
+    double m_sparsity;
+    iodistributions m_distio = TWOD;
+    uint m_compute_error;
+    int m_num_k_blocks;
+    static const int kprimeoffset = 17;
+    normtype m_input_normalization;
+
+    void parseCommandLine() {
+      ParseCommandLine pc(this->m_argc, this->m_argv);
+      pc.parseplancopts();
+      this->m_Afile_name = pc.input_file_name();
+      this->m_pr = pc.pr();
+      this->m_sparsity = pc.sparsity();
+      this->m_num_it = pc.iterations();
+      this->m_distio = TWOD;
+      this->m_regW = pc.regW();
+      this->m_regH = pc.regH();
+      this->m_num_k_blocks = 1;
+      this->m_globalm = pc.globalm();
+      this->m_globaln = pc.globaln();
+      this->m_compute_error = pc.compute_error();
+      this->m_distio = TWOD;
+      this->m_input_normalization = pc.input_normalization();
+      pc.printConfig();
+    }
+
+  public:
+
+    HierNMFDriver(int argc, char * argv[]) {
+      this->m_argc = argc;
+      this->m_argv = argv;
+      this->parseCommandLine();
+    }
+};
+
+int main(int argc, char * argv[]) {
+  HierNMFDriver(argc,argv);
 }
+
