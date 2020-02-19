58841f4afcad59de37cd363f2ad6261e0dcac99a
Lawton Manning <mannlg15@wfu.edu>
2020-02-19 12:15:32 -0500

    fixed matrix print

--- hiernmf/matutils.hpp
+++ hiernmf/matutils.hpp
@@ -1,25 +1,21 @@
 
-using namespace planc;
+namespace planc {
+  template <class INPUTMATTYPE>
+    void print(INPUTMATTYPE M, const char * name) {
+      int size, rank;
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-template <class INPUTMATTPYE>
-void print(INPUTMATTYPE M, const char * name) {
-  int size, rank;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  
-  MPI_Barrier(MPI_COMM_WORLD);
-  if (rank == 0) {
-  fprintf(name, "%s:", name);
-  M.print(name);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-  for (int i = 1; i < size; i++) {
-    if (i == rank) {
-      M.print();
+      MPI_Barrier(MPI_COMM_WORLD);
+      if (rank == 0) {
+        M.print(name);
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
+      for (int i = 1; i < size; i++) {
+        if (i == rank) {
+          M.print();
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+      }
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-  }
-}
-
-
-  
+} 
